d43498bf0bb7f52b890a48ef2d854ffcb4413cd3
ICAROCAETANO <icaro.caetano@gmail.com>
2026-08-19 22:52:29 -0300

    first commit

---
+++ src/main/java/com/project/poc_method_references/PocMethodReferencesApplication.java
@@ -0,0 +1,13 @@
+package com.project.poc_method_references;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocMethodReferencesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocMethodReferencesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/poc_method_references/PocMethodReferencesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.poc_method_references;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocMethodReferencesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
