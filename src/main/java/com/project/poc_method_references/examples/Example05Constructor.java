659cda8443714b4363b7971f1a0b2c5708bf8e5d
ICAROCAETANO <icaro.caetano@gmail.com>
2026-08-25 23:01:41 -0300

    feat: add exmaple 5

---
+++ src/main/java/com/project/poc_method_references/examples/Example05Constructor.java
@@ -0,0 +1,27 @@
+package com.project.poc_method_references.examples;
+
+
+import com.project.poc_method_references.model.Person;
+
+import java.util.function.BiFunction;
+
+public class Example05Constructor {
+
+    public static void main(String[] args) {
+
+        // Com Lambda
+        BiFunction<String, Integer, Person> lambda =
+                (name, age) -> new Person(name, age);
+
+        Person person1 = lambda.apply("Joao", 30);
+
+        System.out.println("Lambda: " + person1);
+
+        // Constructor Reference
+        BiFunction<String, Integer, Person> constructorReference = Person::new;
+
+        Person person2 = constructorReference.apply("Maria", 25);
+
+        System.out.println("Constructor Reference: " + person2);
+    }
+}
