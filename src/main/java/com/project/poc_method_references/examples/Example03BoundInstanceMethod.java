3527c55bc59df991fb5429e0e3d7b5e55bf02f33
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2026-08-28 11:09:21 -0300

    Atualizar o Example03BoundInstanceMethod.java

--- src/main/java/com/project/poc_method_references/examples/Example03BoundInstanceMethod.java
+++ src/main/java/com/project/poc_method_references/examples/Example03BoundInstanceMethod.java
@@ -10,12 +10,10 @@ public class Example03BoundInstanceMethod {
 
         Person person = new Person("Joao", 30);
 
-        // Com Lambda
         Supplier<String> lambda = () -> person.getName();
 
         System.out.println("Lambda: " + lambda.get());
 
-        // Method Reference
         Supplier<String> methodReference = person::getName;
 
         System.out.println("Method Reference: " + methodReference.get());
